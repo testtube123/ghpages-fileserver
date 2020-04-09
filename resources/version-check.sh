a386dea708ba46bc01c34721f105a9611bc64b93
testtube123 <goodaythatismyname@gmail.com>
2020-04-09 12:43:37 -0400

    more

--- resources/version-check.sh
+++ resources/version-check.sh
@@ -1,4 +1,4 @@
-imple script to list version numbers of critical development tools
+#Simple script to list version numbers of critical development tools
 export LC_ALL=C
 bash --version | head -n1 | cut -d" " -f2-4
 MYSH=$(readlink -f /bin/sh)
